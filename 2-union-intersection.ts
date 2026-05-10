03d28bdbd2481701e0aab105ea2e44b800d70db6
MohamedArshath10 <arshath.m2003@gmail.com>
2026-05-10 19:38:05 +0530

    Initialized Typescript

---
+++ 2-union-intersection.ts
@@ -0,0 +1,11 @@
+// union --> | otherwise said as OR
+let newValue: number | string = "hello"
+console.log(newValue)
+
+
+// intersection --> & otherwise said as AND
+let obj: {name: string} & {age: number} = {
+    name: "Arshath",
+    age: 23
+}
+console.log(obj)
